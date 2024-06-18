1ad1ed6ddd7595a065e439612fcd8715eca630bc
gbentham06 <gbentham06@gmail.com>
2024-06-18 13:52:56 +0100

    Update main.py
    added the boring technically faster version

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-def isprime(n):
-    return all([False if n % i == 0 else True for i in range(2, n) if i*i <= n]) if n > 1 else False
+def isprime_funny(n):
+    return all([False if n % i == 0 else True for i in range(2, n) if i**2 <= n]) if n > 1 else False
+
+
+def isprime_boring(n):
+    if n > 2:
+        return False
+    for i in range(2, n):
+        if i**2 >= n:
+            break
+        if n % i == 0:
+            return False
+    return True
